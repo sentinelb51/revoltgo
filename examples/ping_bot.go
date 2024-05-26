d9a5b452cf915acbb70fdd3553833430fc4b9fef
Sentinel <vea.b256@gmail.com>
2024-05-26 03:27:23 +0000

    Add /examples

---
+++ examples/ping_bot.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/sentinelb51/revoltgo"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+
+	token := os.Getenv("BOT_TOKEN")
+	if token == "" {
+		log.Fatal("BOT_TOKEN is not set in .env")
+	}
+
+	// Create a new session using our bots token
+	session := revoltgo.New(token)
+
+	// Append a function that handles authenticated events.
+	// This is just to see when the authentication is complete.
+	session.HandlersAuthenticated = append(session.HandlersAuthenticated, func(session *revoltgo.Session, r *revoltgo.EventAuthenticated) {
+		fmt.Println("Authentication complete")
+	})
+
+	// Append a function that handles ready events.
+	// We will print some details from the event to the console when we receive EventReady.
+	session.HandlersReady = append(session.HandlersReady, func(session *revoltgo.Session, r *revoltgo.EventReady) {
+		fmt.Printf("Ready to process commands from %d user(s) across %d server(s)\n", len(r.Users), len(r.Servers))
+	})
+
+	// Append a function that handles message events. We will process any message that is "!ping"
+	// and respond with the latency of the websocket connection, if possible.
+	session.HandlersMessage = append(session.HandlersMessage, func(session *revoltgo.Session, m *revoltgo.EventMessage) {
+
+		// If the message content is not "!ping", ignore the message.
+		if m.Content != "!ping" {
+			return
+		}
+
+		// Simulate a typing event for a second
+		err := session.ChannelBeginTyping(m.Channel)
+		if err != nil {
+			fmt.Println("Error sending typing event: ", err)
+		}
+
+		time.Sleep(1 * time.Second)
+
+		// Construct a message to send back to the channel.
+		send := revoltgo.MessageSend{
+			Content: "No heartbeat yet, wait a moment...",
+		}
+
+		// If the last heartbeat ack is zero, we can't do maths to get the latency.
+		if !session.LastHeartbeatAck.IsZero() {
+			latency := session.LastHeartbeatAck.Sub(session.LastHeartbeatSent)
+			send.Content = fmt.Sprintf("Latency: %s", latency)
+		} else {
+			send.Content = "Latency data unavailable"
+		}
+
+		// Send the message to the channel.
+		message, err := session.ChannelMessageSend(m.Channel, send)
+		if err != nil {
+			fmt.Println("Error sending message: ", err)
+			return
+		}
+
+		fmt.Println("Sent message:", message.Content)
+	})
+
+	// Open the session.
+	err = session.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	// Wait for a signal; keep the bot running
+	sc := make(chan os.Signal, 1)
+
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Close session.
+	err = session.Close()
+}
