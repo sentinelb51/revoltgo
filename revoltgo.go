b5c7a2976d40bd06ca25d01e5a36f94c35b1e42a
sentinelb51 <vea.b256@gmail.com>
2025-12-29 23:21:39 +0000

    Update version to 3.0 beta 1

--- revoltgo.go
+++ revoltgo.go
@@ -3,7 +3,9 @@ Package revoltgo is a Go wrapper for the Revolt API with low-level bindings
 
 	Made by @sentinelb51
 	For support, join our revolt server on the GitHub README file
+
+Sometimes I will leave TO-DO comments for things I want to globally change later.
 */
 package revoltgo
 
-const VERSION = "v2.6.0"
+const VERSION = "v3.0.0-beta.1"
