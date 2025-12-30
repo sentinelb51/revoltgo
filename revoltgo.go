47bef08a4f85cbb29a05659202902bcf82506309
sentinelb51 <vea.b256@gmail.com>
2025-12-30 22:29:32 +0000

    Convert into defined structs rather than inlined

--- revoltgo.go
+++ revoltgo.go
@@ -11,36 +11,48 @@ package revoltgo
 const VERSION = "v3.0.0-beta.2"
 
 type RootData struct {
-	Revolt   string `json:"revolt"`
-	Features struct {
-		Captcha struct {
-			Enabled bool   `json:"enabled"`
-			Key     string `json:"key"`
-		} `json:"captcha"`
-		Email      bool `json:"email"`
-		InviteOnly bool `json:"invite_only"`
-		Autumn     struct {
-			Enabled bool   `json:"enabled"`
-			URL     string `json:"url"`
-		} `json:"autumn"`
-		January struct {
-			Enabled bool   `json:"enabled"`
-			URL     string `json:"url"`
-		} `json:"january"`
-		Voso struct {
-			Enabled bool   `json:"enabled"`
-			URL     string `json:"url"`
-			WS      string `json:"ws"`
-		} `json:"voso"`
-	} `json:"features"`
-	WS    string `json:"ws"`
-	App   string `json:"app"`
-	VapID string `json:"vapid"`
-	Build struct {
-		CommitSha       string `json:"commit_sha"`
-		CommitTimestamp string `json:"commit_timestamp"`
-		SemVer          string `json:"semver"`
-		OriginURL       string `json:"origin_url"`
-		Timestamp       string `json:"timestamp"`
-	} `json:"build"`
+	WS       string           `json:"ws"`
+	App      string           `json:"app"`
+	VapID    string           `json:"vapid"`
+	Revolt   string           `json:"revolt"`
+	Build    RootDataBuild    `json:"build"`
+	Features RootDataFeatures `json:"features"`
+}
+
+type RootDataFeaturesCaptcha struct {
+	Enabled bool   `json:"enabled"`
+	Key     string `json:"key"`
+}
+
+type RootDataFeaturesAutumn struct {
+	Enabled bool   `json:"enabled"`
+	URL     string `json:"url"`
+}
+
+type RootDataFeaturesJanuary struct {
+	Enabled bool   `json:"enabled"`
+	URL     string `json:"url"`
+}
+
+type RootDataFeaturesVoso struct {
+	Enabled bool   `json:"enabled"`
+	URL     string `json:"url"`
+	WS      string `json:"ws"`
+}
+
+type RootDataFeatures struct {
+	Captcha    RootDataFeaturesCaptcha `json:"captcha"`
+	Email      bool                    `json:"email"`
+	InviteOnly bool                    `json:"invite_only"`
+	Autumn     RootDataFeaturesAutumn  `json:"autumn"`
+	January    RootDataFeaturesJanuary `json:"january"`
+	Voso       RootDataFeaturesVoso    `json:"voso"`
+}
+
+type RootDataBuild struct {
+	CommitSha       string `json:"commit_sha"`
+	CommitTimestamp string `json:"commit_timestamp"`
+	SemVer          string `json:"semver"`
+	OriginURL       string `json:"origin_url"`
+	Timestamp       string `json:"timestamp"`
 }
