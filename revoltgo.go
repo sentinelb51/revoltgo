e0f849b92dd3ec6cafbbfa16afb8e26024a59f8d
sentinelb51 <vea.b256@gmail.com>
2026-01-02 11:02:03 +0000

    Update version to beta 4

--- revoltgo.go
+++ revoltgo.go
@@ -8,7 +8,7 @@ Sometimes I will leave TO-DO comments for things I want to globally change later
 */
 package revoltgo
 
-const VERSION = "v3.0.0-beta.3"
+const VERSION = "v3.0.0-beta.4"
 
 type RootData struct {
 	WS       string           `msg:"ws" json:"ws,omitempty"`
