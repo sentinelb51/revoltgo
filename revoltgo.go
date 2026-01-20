9938a6df96ce7a9375d8316f5e47185ea67c6a5c
sentinelb51 <vea.b256@gmail.com>
2026-01-20 21:31:23 +0000

    Increment version

--- revoltgo.go
+++ revoltgo.go
@@ -8,7 +8,7 @@ Sometimes I will leave TO-DO comments for things I want to globally change later
 */
 package revoltgo
 
-const VERSION = "v3.0.0-beta.7"
+const VERSION = "v3.0.0-beta.8"
 
 type RootData struct {
 	WS       string           `msg:"ws" json:"ws,omitempty"`
